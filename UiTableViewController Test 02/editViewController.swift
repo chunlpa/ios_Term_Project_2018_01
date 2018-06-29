cd2441f56d156d28a259f99d45bb412a2244b52f
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-06-29 11:18:17 +0900

    plus time

---
+++ UiTableViewController Test 02/editViewController.swift
@@ -0,0 +1,35 @@
+//
+//  editViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7703_08 on 2018. 6. 29..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class editViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UiTableViewController Test 02/timeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  timeViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7703_08 on 2018. 6. 29..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class timeViewController: UIViewController {
+    let timeSelector: Selector = #selector(timeViewController.updateTime)
+    let tnterval = 1.0
+    var count = 0
+    
+    
+    
+    @IBOutlet weak var currenttime: UILabel!
+    @IBOutlet weak var pickertime: UILabel!
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Timer.scheduledTimer(timeInterval: tnterval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        pickertime.text =
+        "선택시간: " + formatter.string(from: datePickerView.date)
+         }
+    @objc func updateTime() {
+   //     currenttime.text = String(count)
+   //     count = count + 1
+        
+        
+        let date = Date()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        currenttime.text = "현재시간:" + formatter.string(from: date as Date)
+    }
+    
+}
